aefd08c7fe19b6eb4d860abbfd50ac6f9de62642
DanielFreitassc <danielfreitassc@gmail.com>
2024-02-20 13:46:28 -0300

    Consumindo api mercado libre

--- reactMercadoLivre/src/App.jsx
+++ reactMercadoLivre/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
+import Products from './components/Products/Products'
 
 function App() {
   return (
     <>
       <Header/>
+      <Products/>
     </>
   )
 }
