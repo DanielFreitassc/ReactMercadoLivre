c3298a71e5a5bc506103acce7d490ec40d522ddf
DanielFreitassc <danielfreitassc@gmail.com>
2024-02-20 17:26:26 -0300

    corrigindo problemas da search bar

--- reactMercadoLivre/src/components/SearchBar/SearchBar.jsx
+++ reactMercadoLivre/src/components/SearchBar/SearchBar.jsx
@@ -4,12 +4,13 @@ import "./SearchBar.css";
 
 function SearchBar() {
     const [searchValue, setSearchValue] = useState("");
+    const handleSearch = () => {
+        alert("teste");
+    };
 
-    
     return (  
-        <form className='search-bar'>
+        <form className='search-bar' onSubmit={handleSearch}>
             <input type="search" value={searchValue} placeholder="Buscar produtos" className="search__input"  onChange={({target}) => setSearchValue(target.value)} required/>
-            {searchValue}
             <button type="submit" className="search__button">
                 <IoSearchOutline/>
             </button>
