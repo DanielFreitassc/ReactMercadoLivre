ec5ea05761d69afd8b5dd2f3f3c1c9675c99958e
DanielFreitassc <danielfreitassc@gmail.com>
2024-02-20 17:58:32 -0300

    Barra de pesquisa funcionando

--- reactMercadoLivre/src/components/SearchBar/SearchBar.jsx
+++ reactMercadoLivre/src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { IoSearchOutline } from "react-icons/io5";
 import "./SearchBar.css";
+import fetchProducts from '../../api/fetchProducts';
+import AppContext from '../../context/AppContext';
 
 function SearchBar() {
+    const {setProducts} = useContext(AppContext);
     const [searchValue, setSearchValue] = useState("");
-    const handleSearch = () => {
-        alert("teste");
+
+
+    const handleSearch = async (event) => {
+        event.preventDefault();
+        
+        const products = await fetchProducts(searchValue);
+        setProducts(products)
+        setSearchValue("");
     };
 
     return (  
         <form className='search-bar' onSubmit={handleSearch}>
-            <input type="search" value={searchValue} placeholder="Buscar produtos" className="search__input"  onChange={({target}) => setSearchValue(target.value)} required/>
+            {name}
+            <input type="search" value={searchValue} 
+            placeholder="Buscar produtos" 
+            className="search__input"  
+            onChange={({target}) => 
+            setSearchValue(target.value)} 
+            required
+            />
+
             <button type="submit" className="search__button">
                 <IoSearchOutline/>
             </button>
@@ -19,3 +36,4 @@ function SearchBar() {
 }
 
 export default SearchBar;
+
