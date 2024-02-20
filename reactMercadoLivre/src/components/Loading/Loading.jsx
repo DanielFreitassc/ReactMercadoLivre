98a01b1e51dfeaefc362d0a0ca319b526531d60d
DanielFreitassc <danielfreitassc@gmail.com>
2024-02-20 17:11:01 -0300

    Criei o componente de animação

---
+++ reactMercadoLivre/src/components/Loading/Loading.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AiOutlineLoading } from "react-icons/ai";
+import "./Loading.css"
+function Loading() {
+    return <AiOutlineLoading className='loading'/>
+}
+
+export default Loading;
