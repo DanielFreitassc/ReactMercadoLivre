cc271ea6eee8e29f744cfdd982381c4970df8a69
DanielFreitassc <danielfreitassc@gmail.com>
2024-02-21 14:05:07 -0300

    Criando o componente carrinho

---
+++ reactMercadoLivre/src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Cart() {
+    return (  
+        <section>
+            <div className='cart-items'>Itens do carrinho</div>
+            <div className='cart-resume'>Resumo do carrinho</div>
+        
+        </section>
+    );
+}
+
+export default Cart;
