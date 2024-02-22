928244ae557ab924efa882a552b7f2f3f57ca208
DanielFreitassc <danielfreitassc@gmail.com>
2024-02-22 16:41:31 -0300

    Fiz a função para remoção de produtos

--- reactMercadoLivre/src/components/CartItem/CartItem.jsx
+++ reactMercadoLivre/src/components/CartItem/CartItem.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import propTypes from "prop-types";
 import "./CartItem.css";
 import { FaRegTrashCan } from "react-icons/fa6";
 import formatCurrency from "../../utils/formatCurrency"
+import AppContext from '../../context/AppContext';
 function CartItem({data}) {
-    const {thumbnail, title, price} = data;
+
+    const {cartItems , setCartItems} = useContext(AppContext);
+    const {id, thumbnail, title, price} = data;
+    const handleRemoveItem = () => {
+        const updatedItems = cartItems.filter((item) => item.id != id);
+        setCartItems(updatedItems);
+
+    }
     return (  
         <section className='cart-item'>
             <img 
@@ -17,7 +25,7 @@ function CartItem({data}) {
                 <h3 className='cart-item-title'>{title}</h3>
                 <h3 className='cart-item-price'>{formatCurrency(price,"BRL")}</h3>
 
-                <button type='button' className='button__remove-item'> 
+                <button type='button' className='button__remove-item' onClick={handleRemoveItem}>  
                 <FaRegTrashCan />
                 </button>
             </div>
